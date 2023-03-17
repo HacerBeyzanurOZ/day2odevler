d2ee7f5ae45f7453007c5c05014002730fd3749a
snowitch <127445257+HacerBeyzanurOZ@users.noreply.github.com>
2023-03-17 21:06:07 +0300

    Add files via upload

---
+++ day2ödev1.py
@@ -0,0 +1,15 @@
+sinif=["hacer" "beyzanur" "rümeysa" ]
+print(sinif)
+
+def öğrenciEkle():
+    isimSoyisim = input("lütfen geçerli isim soyisim giriniz: ")
+    sinif(isimSoyisim)
+    sinif.append(isimSoyisim)
+    sinif.index(isimSoyisim)
+    print("eklendi")
+
+
+def öğrenciSil():
+    isimSoyisim =
+
+
---
+++ day2ödev2.py
@@ -0,0 +1,20 @@
+class öğrenciListesi:
+    öğrenci=["hacer","beyza"]
+    def __init__(self,isimSoyisim): 
+        self.isimSoyisim = isimSoyisim
+       
+    def öğrenciEkle():
+        isimSoyisim = input("lütfen geçerli bir isim soyisim girin: ")
+
+    def ogrenciEkle(self):
+        self.öğrenci.append(self.isim)
+        print("[]adlı kişi öğrenci listesine eklendi"),
+
+
+
+
+#burada izlediğimiz class dersini ilk ödevle ortak yapmaya çalıştım ama olmadı :,)
+
+
+
+
